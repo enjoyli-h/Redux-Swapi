abf11e5ba27747d1b41d77587b9860541833fab5
Enjoyli Harrell <joli@Enjoylis-iMac.local>
2018-04-23 21:30:01 -0700

    Called Dependencies in index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import starWarsReducer from './reducers';
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(starWarsReducer/* rootReducer */, applyMiddleware/* applyMiddleware goes here */);
 
 ReactDOM.render(
   <Provider store={store}>
